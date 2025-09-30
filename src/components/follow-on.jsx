feb0a0726588a0847a0b3acac5d3d8458851b174
anwar9136 <191209710+anwar9136@users.noreply.github.com>
2025-09-30 21:41:22 +0530

    added linkedin in follow-on

--- src/components/follow-on.jsx
+++ src/components/follow-on.jsx
@@ -1,4 +1,4 @@
-import {FaInstagram, FaXTwitter, FaYoutube} from "react-icons/fa6";
+import {FaLinkedin} from "react-icons/fa6";
 
 const FollowOn = () => {
   return (
@@ -7,15 +7,15 @@ const FollowOn = () => {
     >
       <span>Follow on:</span>
       <div className="flex gap-4 pt-3">
-        <a href="https://www.youtube.com/roadsidecoder">
-          <FaYoutube size={20} />
+        <a href="https://www.linkedin.com/in/anwar-basha-696796284/">
+          <FaLinkedin size={20} />
         </a>
-        <a href="https://www.instagram.com/roadsidecoder">
+        {/* <a href="https://www.instagram.com/roadsidecoder">
           <FaInstagram size={20} />
         </a>
         <a href="https://www.twitter.com/piyush_eon">
           <FaXTwitter size={20} />
-        </a>
+        </a> */}
       </div>
     </div>
   );
